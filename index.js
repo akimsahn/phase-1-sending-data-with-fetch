dddeebd68c818ac0c01ffc332c3060558dfa1dca
Allison Kim <akeem159@yahoo.com>
2022-07-22 09:41:19 -0400

    commit tree 24a8e710a233acc1a9740b8145d7d08aa2072784

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-// Add your code here
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form')
+    form.addEventListener('submit',submitEventData)
+})
+
+function submitEventData(e) {
+    e.preventDefault()
+    const username = e.target.username.value
+    const email = e.target.email.value
+    submitData(username,email)
+    e.target.reset()
+}
+
+function submitData(userName, userEmail) {
+    const configureUser = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        }),
+    }
+
+    return fetch("http://localhost:3000/users", configureUser)
+    .then(resp => resp.json())
+    .then(data => appendID(data))
+    .catch(err => appendError(err))
+}
+
+function displayNewUser(input) {
+    const table = document.querySelector('table')
+    const newRow = document.createElement('tr')
+    const username = document.createElement('td')
+    const email = document.createElement('td')
+    username.textContent = input.name
+    email.textContent = input.email
+    console.log(input)
+    newRow.appendChild(username)
+    newRow.appendChild(email)
+    table.append(newRow)
+}
+
+function appendID(input) {
+    const body = document.querySelector('body')
+    body.append(input.id)
+}
+
+function appendError(err) {
+    const body = document.querySelector('body')
+    body.append(err)
+}
